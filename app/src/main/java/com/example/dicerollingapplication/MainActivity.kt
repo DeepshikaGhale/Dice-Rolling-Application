bdf85d3626a49690e35906d0dcd90a75485bcf97
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-04-15 18:22:26 -0400

    Allow user to create dice and validate dice size

--- app/src/main/java/com/example/dicerollingapplication/MainActivity.kt
+++ app/src/main/java/com/example/dicerollingapplication/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.dicerollingapplication
 
 import android.content.Context
-import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,13 +9,17 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.example.dicerollingapplication.databinding.ActivityMainBinding
-import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
+    //static variables
     private val placeholder: String = "Choose"
-    private var diceList = arrayListOf<Any>()
+    private val localStorage: String = "Preferences"
+    //list to store dice
+    private var diceList : ArrayList<Any>  = arrayListOf<Any>()
+
+    //stores the value of dice selected by user
     var selectedDice = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +29,6 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
         //call data from shared-preferences
         readStoredDie()
-//        storeUserDie()
 
         //adapter for spinner, the create from resources method is used to create an dropdown using the array of created in the resource file
         val adapter = ArrayAdapter(this, R.layout.spinner_item, diceList)
@@ -60,6 +62,7 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
                 binding.resultSection.visibility = View.VISIBLE
                 binding.result2Id.visibility = View.VISIBLE
                 binding.result2Id.visibility = View.VISIBLE
+                binding.resultLabelId.text = "Random side up for dice $selectedDice is:"
 
                 val die = Die(noOfSides = selectedDice)
                 die.roll()
@@ -69,6 +72,25 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
             }else{
                 Toast.makeText(this, "Please select dice to roll", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        //create dice by user
+        binding.createDice.setOnClickListener(){
+            var userDice = binding.dice.text.toString()
+            if (userDice.isNotEmpty()){
+                //user can add the dice only if the user given dice size is not already present
+                if(!validateUserDiceSize(userDice.toInt())){
+                    val key = diceList.size - 1
+                    storeUserDie(key, userDice.toInt())
+                    binding.dice.text.clear()
+                    Toast.makeText(this, "Your dice has been added.", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this, "Given dice size is already present.", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "Please enter a dice size", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -89,49 +111,70 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     //store user created die
     private fun storeUserDie(key: Int, die: Int){
         //initialize shared preference
-        val sharedPref = getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
+        val sharedPref = getSharedPreferences(localStorage, Context.MODE_PRIVATE)
         //to store the value of user created die
         val editor = sharedPref.edit()
-        editor.putString(key.toString(),die.toString())
+        editor.putInt(key.toString(),die)
         editor.apply()
+        diceList += die //adding die to the list
     }
 
     //read user created die
-    private fun readStoredDie(): ArrayList<Any>{
+    private fun readStoredDie(){
         diceList.clear() // clearing list before adding the stored data
         diceList.add(placeholder) //re-adding the placeholder in the first index
 
         //initialize shared preference
-        val sharedPref = getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
+        val sharedPref = getSharedPreferences(localStorage, Context.MODE_PRIVATE)
         //to retrieve data
         val allEntries = sharedPref.all
 
         if (allEntries.isEmpty()){
             addDefaultDice()
+            for(i in 0..allEntries.size-1){
+                val data = sharedPref.getInt(i.toString(), 0)
+                //add dice from the local storage to list
+                diceList.add(data)
+            }
         }
 
         for(i in 0..allEntries.size-1){
-           val data = sharedPref.getString(i.toString(), "")
-           //add dice from the local storage to list
-           diceList.add(data.toString().toInt())
-           Log.d("entries-data", data.toString())
-        }
+            Log.d("allentries", allEntries.toString())
 
-        Log.d("data-size", allEntries.size.toString())
-        return diceList
+            val data = sharedPref.getInt(i.toString(), 0)
+           //add dice from the local storage to list
+           diceList.add(data)
+
+        }
     }
 
     //to store the data when app starts for the first time
     private fun addDefaultDice(){
         var initialDiceList = arrayListOf<Int>(4,6,8,10,12,20)
         //initialize shared preference
-        val sharedPref = getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
+        val sharedPref = getSharedPreferences(localStorage, Context.MODE_PRIVATE)
         //to store the value of user created die
         val editor = sharedPref.edit()
         //to store the value of when the app runs for the first time
             for ( i in initialDiceList.indices){
                 editor.putInt(i.toString(), initialDiceList[i].toString().toInt())
+                editor.apply()
             }
+
+        val allEntries = sharedPref.all
+        Log.d("size-after-adding", allEntries.size.toString())
+    }
+
+    //validate if the user dice size is already present
+    private fun validateUserDiceSize(dice: Int): Boolean{
+
+        var hasDice = diceList.contains(dice)
+
+        if(hasDice){
+            Toast.makeText(this, "Please enter a different dice", Toast.LENGTH_SHORT).show()
+        }
+
+        return hasDice
     }
 
 }
