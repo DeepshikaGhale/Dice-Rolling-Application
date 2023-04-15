425de1730ac524f7536a5b882bbb054a55b42a19
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-04-15 13:53:08 -0400

    Create Die class

---
+++ app/src/main/java/com/example/dicerollingapplication/Die.kt
@@ -0,0 +1,33 @@
+package com.example.dicerollingapplication
+
+import kotlin.random.Random
+
+class Die {
+    //define variables
+    var noOfSides : Int = 0
+    var randomSideUp : Int = 0
+
+    //primary constructor with zero arguments
+    constructor(){
+        noOfSides = 6
+        roll()
+    }
+
+    //secondary constructors
+    //takes the number of sides value from the user
+    constructor(noOfSides: Int): this(){
+        this.noOfSides = noOfSides
+        roll()
+    }
+
+    //takes both name and number of sides from the user
+    constructor(name: String, noOfSides: Int): this(){
+        this.noOfSides = noOfSides
+        roll()
+    }
+
+    //provides random value to randomSideUp
+    fun roll(){
+        randomSideUp = Random.nextInt(1, noOfSides+1)
+    }
+}
